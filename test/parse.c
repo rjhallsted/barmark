81c6c33f3d8ecf8ad8a8ad0e03f8cac9cdde2af7
rjhallsted <rjhallsted@gmail.com>
2023-05-01 07:18:40 -0700

    add other produce_code_block tests

--- test/parse.c
+++ test/parse.c
@@ -56,7 +56,69 @@ void test_produce_code_block_double_newline_ending(void) {
   ast_free_node(actual);
 }
 
+void test_produce_code_block_newline_missing_tab_ending(void) {
+  Token *tokens[14] = {
+      newToken(&(SYMBOLS[SYMBOL_TAB_ID]), "\t"),
+      newToken(&(SYMBOLS[SYMBOL_TEXT_ID]), "foobar"),
+      newToken(&(SYMBOLS[SYMBOL_SPACE_ID]), " "),
+      newToken(&(SYMBOLS[SYMBOL_TEXT_ID]), "was"),
+      newToken(&(SYMBOLS[SYMBOL_SPACE_ID]), " "),
+      newToken(&(SYMBOLS[SYMBOL_TEXT_ID]), "here"),
+      newToken(&(SYMBOLS[SYMBOL_NL_ID]), "\n"),
+      newToken(&(SYMBOLS[SYMBOL_TEXT_ID]), "also"),
+      newToken(&(SYMBOLS[SYMBOL_SPACE_ID]), " "),
+      newToken(&(SYMBOLS[SYMBOL_TEXT_ID]), "here"),
+      newToken(&(SYMBOLS[SYMBOL_NL_ID]), "\n"),
+      newToken(&(SYMBOLS[SYMBOL_NL_ID]), "\n"),
+      newToken(&(SYMBOLS[SYMBOL_TEXT_ID]), "not_included"),
+  };
+
+  Token *stream = join_token_array(tokens, 14);
+  Token **stream_ptr = &stream;
+  ASTNode *expected = ast_create_node(ASTN_CODE_BLOCK);
+  expected->contents = strdup("foobar was here");
+  Token *expected_ptr = advance_token_list_by(*stream_ptr, 6);
+
+  ASTNode *actual = produce_code_block(stream_ptr);
+  assert_ast_nodes_equal(expected, actual);
+  TEST_ASSERT_EQUAL_PTR(expected_ptr, *stream_ptr);
+
+  free_token_list(stream);
+  ast_free_node(expected);
+  ast_free_node(actual);
+}
+
+void test_produce_code_block_end_of_stream_ending(void) {
+  Token *tokens[10] = {
+      newToken(&(SYMBOLS[SYMBOL_TAB_ID]), "\t"),
+      newToken(&(SYMBOLS[SYMBOL_TEXT_ID]), "foobar"),
+      newToken(&(SYMBOLS[SYMBOL_SPACE_ID]), " "),
+      newToken(&(SYMBOLS[SYMBOL_TEXT_ID]), "was"),
+      newToken(&(SYMBOLS[SYMBOL_SPACE_ID]), " "),
+      newToken(&(SYMBOLS[SYMBOL_TEXT_ID]), "here"),
+      newToken(&(SYMBOLS[SYMBOL_NL_ID]), "\n"),
+      newToken(&(SYMBOLS[SYMBOL_TAB_ID]), "\t"),
+      newToken(&(SYMBOLS[SYMBOL_TEXT_ID]), "also"),
+      newToken(&(SYMBOLS[SYMBOL_SPACE_ID]), " "),
+  };
+
+  Token *stream = join_token_array(tokens, 10);
+  Token **stream_ptr = &stream;
+  ASTNode *expected = ast_create_node(ASTN_CODE_BLOCK);
+  expected->contents = strdup("foobar was here\nalso ");
+
+  ASTNode *actual = produce_code_block(stream_ptr);
+  assert_ast_nodes_equal(expected, actual);
+  TEST_ASSERT_NULL(*stream_ptr);
+
+  free_token_list(stream);
+  ast_free_node(expected);
+  ast_free_node(actual);
+}
+
 void parseTests(void) {
   printf("running parse tests\n");
   RUN_TEST(test_produce_code_block_double_newline_ending);
+  RUN_TEST(test_produce_code_block_newline_missing_tab_ending);
+  RUN_TEST(test_produce_code_block_end_of_stream_ending);
 }
