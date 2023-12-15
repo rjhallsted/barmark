3a6a170cc6d63256ef2adc200227d18a19659cd2
rjhallsted <rjhallsted@gmail.com>
2023-12-15 11:27:32 +0100

    Add test for reverse_dl_list

--- test/util.c
+++ test/util.c
@@ -29,6 +29,7 @@ void test_str_append_with_nl(void) {
 
 typedef struct TestList {
   struct TestList *next;
+  struct TestList *prev;
   int value;
 } TestList;
 
@@ -55,6 +56,31 @@ void test_reverse_list(void) {
   free(c);
 }
 
+void test_reverse_dl_list(void) {
+  TestList *a = malloc(sizeof(TestList));
+  a->value = 1;
+  TestList *b = malloc(sizeof(TestList));
+  b->value = 2;
+  TestList *c = malloc(sizeof(TestList));
+  c->value = 3;
+  TestList *head = (TestList *)add_item_to_dl_list((DoublyLinkedItem *)NULL,
+                                                   (DoublyLinkedItem *)a);
+  head = (TestList *)add_item_to_dl_list((DoublyLinkedItem *)head,
+                                         (DoublyLinkedItem *)b);
+  head = (TestList *)add_item_to_dl_list((DoublyLinkedItem *)head,
+                                         (DoublyLinkedItem *)c);
+
+  TestList *new_head = (TestList *)reverse_dl_list((DoublyLinkedItem *)c);
+  TEST_ASSERT_EQUAL(1, new_head->value);
+  TEST_ASSERT_EQUAL(1, new_head->next->prev->value);
+  TEST_ASSERT_EQUAL(2, new_head->next->value);
+  TEST_ASSERT_EQUAL(2, new_head->next->next->prev->value);
+  TEST_ASSERT_EQUAL(3, new_head->next->next->value);
+  free(a);
+  free(b);
+  free(c);
+}
+
 void util_tests(void) {
   printf("--util tests\n");
   printf("---str_append\n");
@@ -62,4 +88,6 @@ void util_tests(void) {
   RUN_TEST(test_str_append_with_nl);
   printf("---reverse_list\n");
   RUN_TEST(test_reverse_list);
+  printf("---reverse_dl_list\n");
+  RUN_TEST(test_reverse_dl_list);
 }
